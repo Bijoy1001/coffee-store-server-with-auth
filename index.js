08fc9f2e4648521797f3c83b5ca7e8ee1a71ae4d
Bijoy1001 <bijoykhan1001@gmail.com>
2024-03-13 06:38:05 +0600

    updated

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('user');
 
     app.get('/coffee', async(req, res)=>{
       const  cursor = coffeeCollection.find();
@@ -75,6 +76,17 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
+
+    // user related apis
+
+    app.post('/user', async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
